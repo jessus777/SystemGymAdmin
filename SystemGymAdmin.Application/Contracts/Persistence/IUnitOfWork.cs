17bcf9cb4a4fe50dd54dad56be58b3d437a0cc90
Jesus Jimenez Rendon <jesusJimenezRE777@outlook.com>
2024-12-19 13:57:46 -0600

    se agreg la interfaz IUnitOfWork

---
+++ SystemGymAdmin.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace SystemGymAdmin.Application.Contracts.Persistence;
+public interface IUnitOfWork
+    : IDisposable
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RollbackTransactionAsync();
+}
